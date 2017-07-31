6caa7cce132a50258d06569001f6eaf4312303b3
Michael Hawkins <BadgerCode@users.noreply.github.com>
2017-07-31 23:18:16 +0100

    Removes weapons from spectators

--- gamemodes/oresgyn/gamemode/sh_player.lua
+++ gamemodes/oresgyn/gamemode/sh_player.lua
@@ -4,6 +4,7 @@ if not plymeta then Error("FAILED TO FIND PLAYER TABLE") return end
 function plymeta:SetSpectator()
     if self:IsSpectator() then return end
 
+    if SERVER then self:StripWeapons() end
     self.SpectatorPos = self:GetPos() + Vector(0, 0, 400)
     self.SpectatorTargetPos = self:GetPos()
     
